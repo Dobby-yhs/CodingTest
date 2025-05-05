8523102a96078841d399cde8d00a1f3883b6720f
Dobby-yhs <jjj1299@naver.com>
2025-05-06 02:05:59 +0900

    Day 14 : Coding Test Practice

---
+++ CodingTestPractice/Day_14.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+// BAEKJOON 4153번 : 직각삼각형
+void checkTriangle(int a, int b, int c)
+{
+	if (a * a == b * b + c * c)
+		cout << "right\n";
+	else
+		cout << "wrong\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int x, y, z;
+	int max = 0;
+
+	while (1)
+	{
+		cin >> x >> y >> z;
+		
+		if (x == 0 && y == 0 && z == 0)
+			break;
+
+		max = std::max({ x, y, z });
+		
+		if (max == x)
+			checkTriangle(x, y, z);
+		else if (max == y)
+			checkTriangle(y, x, z);
+		else if (max == z)
+			checkTriangle(z, x, y);
+	}
+
+	return 0;
+}
+*/
