02e2df0dd80ca667e5c2957ab73d39b20b38db17
Dobby-yhs <jjj1299@naver.com>
2025-05-27 01:32:47 +0900

    Day 22 : Coding Test Practice

--- CodingTestPractice/Day_22.cpp
+++ CodingTestPractice/Day_22.cpp
@@ -6,7 +6,34 @@
 //using namespace std;
 
 /*
-// BAEKJOON 25305번 : 커트라인
+// BAEKJOON 25305번 : 커트라인 - 내림차순 정렬
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, k;
+	vector<int> range;
+
+	cin >> N >> k;
+
+	while (N--)
+	{
+		int score;
+		cin >> score;
+		range.push_back(score);
+	}
+
+	sort(range.begin(), range.end(), greater<int>());
+
+	cout << range[k - 1];
+
+	return 0;
+}
+*/
+
+/*
+// BAEKJOON 25305번 : 커트라인 - 오름차순 정렬
 int main()
 {
 	ios_base::sync_with_stdio(false);
